57f837232ed542fb4f2672bbd4b66d32f002ca19
shrx <differenxe@gmail.com>
2012-10-23 18:11:53 +0200

    remove CPU temp spikes; other fixes

--- cronskripte/load.sh
+++ cronskripte/load.sh
@@ -15,15 +15,21 @@ zad=$(echo ${z#*,})
 datum=$(date '+%Y/%m/%d %H:%M:%S')
 
 load=$(uptime)
-temp=$(/opt/vc/bin/vcgencmd measure_temp)
+temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+while [ $temp -gt 100000 -o $temp -lt 0 ]; do
+	temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+done
 
 load=$(echo ${load##*: })
 load=$(echo ${load#*, })
 load=$(echo "${load%%,*}*100" | bc)
 load=$(printf "%0.f" $load)
 
-temp=$(echo ${temp##*=})
-temp=$(echo ${temp%%\'*})
+temp=$(echo "$temp/1000.0" | bc -l)
+temp=$(printf "%.1f" $temp)
+if [ "$temp" == "85.0" ]; then
+	temp="$(echo ${zad##*,})"
+fi
 
 if [ "$pred" == "$zad" -a "$zad" == "$load,$temp" ]; then
 	sed -i "$l""d" "$file"
